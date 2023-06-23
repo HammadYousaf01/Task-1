940ba0cc90d882c5dad59a7eac2508dedf038b3b
HammadYousaf01 <hammad.yousaf@arbisoft.com>
2023-06-23 11:50:24 +0500

    Added validations without debounce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import './css/theme-btn.css';
+import './css/validations.css';
 import './theme.js';
 import './validations.js';
 
--- src/validations.js
+++ src/validations.js
@@ -3,3 +3,94 @@ const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
+
+username.addEventListener("input", e => {
+    const usernameError = document.querySelector("#username-error");
+
+    if (e.target.value.length < 4) {
+        addErrorBorder(username);
+        reportError(usernameError, "Username must be longer than 4 characters");
+    } else if (e.target.value.length > 20) {
+        addErrorBorder(username);
+        reportError(usernameError, "Username must be shorter than 20 characters");
+    } else {
+        addSuccessBorder(username);
+        removeError(usernameError);
+    }
+})
+
+email.addEventListener("input", e => {
+    const emailError = document.querySelector("#email-error");
+
+    if (validateEmail(e.target.value)) {
+        addSuccessBorder(email);
+        removeError(emailError);
+    } else {
+        addErrorBorder(email);
+        reportError(emailError, "Email is invalid")
+    }
+})
+
+
+password.addEventListener("input", e => {
+    const passwordError = document.querySelector("#password-error");
+
+    if (e.target.value.length < 8) {
+        reportError(passwordError, "Password needs to be longer than 8 characters");
+        addErrorBorder(password)
+    } else {
+        removeError(passwordError);
+        addSuccessBorder(password);
+    }
+})
+
+
+confirmPassword.addEventListener("input", e => {
+    const confirmPasswordError = document.querySelector("#confirm-password-error");
+    if (e.target.value !== password.value) {
+        reportError(confirmPasswordError, "Passwords need to match");
+        addErrorBorder(confirmPassword);
+    } else {
+        removeError(confirmPasswordError);
+        addSuccessBorder(confirmPassword);
+    }
+
+    
+})
+
+function reportError(element, message) {
+    element.textContent = message;
+    removeClass(element, "none-display");
+    addClass(element, "error-message");
+}
+
+function removeError(element) {
+    removeClass(element, "none-display");
+    element.textContent = "";
+}
+
+function addErrorBorder(element) {
+    addClass(element, "error-border");
+    removeClass(element, "success-border");
+}
+
+function addSuccessBorder(element) {
+    addClass(element, "success-border");
+    removeClass(element, "error-border");
+}
+
+function addClass(element, className) {
+    element.classList.add(className);
+}
+
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
+function validateEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
