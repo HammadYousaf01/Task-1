08ae0d543dad32632857425172e91441b5929d01
HammadYousaf01 <hammad.yousaf@arbisoft.com>
2023-06-25 15:33:49 +0500

    Added form validations

--- src/validations.js
+++ src/validations.js
@@ -1,6 +1,6 @@
 import debounce from './debounce';
 
-const submitBtn = document.querySelector("#submit");
+const form = document.querySelector("form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
@@ -11,58 +11,86 @@ email.addEventListener("input", applyEmailValidations);
 password.addEventListener("input", applyPasswordValidations);
 confirmPassword.addEventListener("input", applyConfirmPasswordValidations);
 
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const usernameValidationsResponse = await applyUsernaneValidations();
+    const emailValidationsResponse = await applyEmailValidations();
+    const passwordValidationsResponse = await applyPasswordValidations();
+    const confirmPasswordValidationsResponse = await applyConfirmPasswordValidations();
+    if (usernameValidationsResponse && emailValidationsResponse && passwordValidationsResponse && confirmPasswordValidationsResponse) {
+        form.submit();
+    } 
+
+})
+
 async function applyUsernaneValidations(e) {
     const usernameError = document.querySelector("#username-error");
 
-    const usernameLength = await debounce(e.target.value.length);
+    const usernameLength = await debounce(username.value.length);
 
     if (usernameLength < 4) {
         addErrorBorder(username);
         reportError(usernameError, "Username must be longer than 4 characters");
+        return false;
     } else if (usernameLength > 20) {
         addErrorBorder(username);
         reportError(usernameError, "Username must be shorter than 20 characters");
+        return false;
     } else {
         addSuccessBorder(username);
         removeError(usernameError);
+        return true;
     }
 }
 
 async function applyEmailValidations(e) {
     const emailError = document.querySelector("#email-error");
 
-    if (validateEmail(await debounce(e.target.value))) {
+    if (validateEmail(await debounce(email.value))) {
         addSuccessBorder(email);
         removeError(emailError);
+        return true;
     } else {
         addErrorBorder(email);
-        reportError(emailError, "Email is invalid")
+        reportError(emailError, "Email is invalid");
+        return false;
     }
 }
 
 async function applyPasswordValidations(e) {
     const passwordError = document.querySelector("#password-error");
 
-    const passwordLength = await debounce(e.target.value.length);
+    if (confirmPassword.value) {
+        await applyConfirmPasswordValidations();
+    }
+
+    const passwordLength = await debounce(password.value.length);
     if (passwordLength < 8) {
         reportError(passwordError, "Password needs to be longer than 8 characters");
         addErrorBorder(password)
+        return false;
     } else {
         removeError(passwordError);
         addSuccessBorder(password);
+        return true;
     }
+
+    
 }
 
 async function applyConfirmPasswordValidations(e) {
     const confirmPasswordError = document.querySelector("#confirm-password-error");
 
-    const confirmPasswordValue = await debounce(e.target.value);
+    const confirmPasswordValue = await debounce(confirmPassword.value);
     if (confirmPasswordValue !== password.value) {
         reportError(confirmPasswordError, "Passwords don't match");
         addErrorBorder(confirmPassword);
+        return false;
     } else {
         removeError(confirmPasswordError);
-        addSuccessBorder(confirmPassword)
+        addSuccessBorder(confirmPassword);
+        return true;
     }
 }
 
